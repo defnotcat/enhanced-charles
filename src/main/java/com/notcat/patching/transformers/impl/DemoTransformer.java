1a5467fa1ed28c0b428781f0de520be33f315503
Cat <32003307+defnotcat@users.noreply.github.com>
2021-07-20 20:33:02 +0200

    removed the popup that randomly freezes the app in place for 5s

--- src/main/java/com/notcat/patching/transformers/impl/DemoTransformer.java
+++ src/main/java/com/notcat/patching/transformers/impl/DemoTransformer.java
@@ -27,6 +27,11 @@ public class DemoTransformer implements ITransformer {
             runnableClass.getDeclaredMethod("run").setBody("{}");
             transformedClasses.add(TransformedClass.from(runnableClass));
 
+            // Random splash window
+            runnableClass = classPool.get("com.xk72.charles.gui.transaction.frames.f");
+            runnableClass.getDeclaredMethod("d").setBody("{if(this.e != null) { this.f = this.e; this.c(null); } this.c($1); this.b($1);}");
+            transformedClasses.add(TransformedClass.from(runnableClass));
+
             return transformedClasses;
 
         } catch(Exception exp) {
